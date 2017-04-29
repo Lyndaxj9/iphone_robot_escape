34a681987ec060b51c061e9250c28a41ab0ad5fc
Lynda <Lyndaxj9@users.noreply.github.com>
2017-04-29 19:38:10 -0400

    got enemy follow movement ai working

--- p07ademola/p07ademola/Enemy.swift
+++ p07ademola/p07ademola/Enemy.swift
@@ -11,7 +11,7 @@ import SpriteKit
 
 class Enemy: SKSpriteNode {
     
-    private var moveSpeed = CGFloat(200)
+    private var moveSpeed = CGFloat(10)
 
     init() {
         let texture = SKTexture(imageNamed: "robot_3Dblue")
@@ -29,23 +29,25 @@ class Enemy: SKSpriteNode {
     
     func followPlayer(scene: SKScene, playerPos: CGPoint) {
         
-        let agroRange = CGFloat(400.0)
+        let agroRange = CGFloat(250.0)
         let distanceX = playerPos.x - self.position.x
         let distanceY = playerPos.y - self.position.y
         
         let distanceTotal = sqrt(distanceX*distanceX + distanceY*distanceY)
         
-        if( distanceTotal <= agroRange ) {
-            let moveX = moveSpeed/distanceX
-            let moveY = moveSpeed/distanceY
+        let v = CGVector(dx:distanceX, dy: distanceY)
+        let angle = atan2(v.dy, v.dx)
+        self.zRotation = angle
+        
+        if( distanceTotal <= self.size.width) {
+            print("too close")
             
-            let newPosX = self.position.x + moveX
-            let newPosY = self.position.y + moveY
-            
-            self.position.x = newPosX
-            self.position.y = newPosY
-            
-            print("follow")
+        } else if( distanceTotal <= agroRange ) {
+            self.position.x += moveSpeed * cos(angle)
+            self.position.y += moveSpeed * sin(angle)
+    
+        } else if( distanceTotal > agroRange ) {
+            print("idle movement")
         }
         //maybe another if statement for when they are super close like width of enemy sprite?
         
